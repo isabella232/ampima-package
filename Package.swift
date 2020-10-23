2911702fe56e7f27e8bcb9ebbecbc68ccd711043
David Cortes <dcortes22@gmail.com>
2020-10-23 11:18:25 -0600

    Add package specification

--- Package.swift
+++ Package.swift
@@ -3,26 +3,30 @@
 
 import PackageDescription
 
+let version = "9.1.0"
+
 let package = Package(
-    name: "AmpIMAPackage",
+    name: "AmpIMA",
+    platforms: [
+        .iOS(.v12), .tvOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "AmpIMAPackage",
-            targets: ["AmpIMAPackage"]),
+            name: "AmpIMA",
+            targets: ["AmpIMA"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/akamai/ampcore-package", from: Version(version)!),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "AmpIMAPackage",
-            dependencies: []),
-        .testTarget(
-            name: "AmpIMAPackageTests",
-            dependencies: ["AmpIMAPackage"]),
+        .binaryTarget(
+            name: "AmpIMA",
+            url: "https://mdtp-a.akamaihd.net/amp-ios-sdk/frameworks/\(version)/AmpIMA.zip",
+            checksum: "55faebf9e7acf373710a538695eea75181997494b95690bceccd62e18a82d8ef"
+        ),
     ]
 )
